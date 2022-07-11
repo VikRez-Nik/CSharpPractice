ddb5db8881af4d1a5f3639b4b1452ac04e1e7c19
DESKTOP-PF5B1E8\spark <viktorreznik116@gmail.com>
2022-07-11 15:07:40 +0300

    no message

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+// Написать консольное приложение,
+// вычисляющие квадратный корень из введённого пользователем числа.
+// Если пользователь ввел не число а текст, выводить сообщение "Ошибка: {введённый текст} не является числом".
+// Если пользователь ввёл отрицательное число - выводить сообщение "Ошибка:  корень отрицательного числа вычислить невозможно".
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int i;
+
+        try { 
+        Console.WriteLine("Введите число для вычисления корня:");
+        string input = Console.ReadLine(); 
+
+        if (double.TryParse(input, out double number)){
+            i = Convert.ToInt32(Math.Sqrt(number));
+            Console.WriteLine(i);}
+        else    { Console.WriteLine("Ошибка: " + input + " не является числом"); }    }
+        
+        catch { Console.WriteLine("Ошибка:  корень отрицательного числа вычислить невозможно"); }
+        }
+    }   
+}
