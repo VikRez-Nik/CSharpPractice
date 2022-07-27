a0759d163d1b6404e9240a264b9319e0a4309b69
DESKTOP-PF5B1E8\spark <viktorreznik116@gmail.com>
2022-07-27 18:31:13 +0300

    18.31

--- AverageLib/Program.cs
+++ AverageLib/Program.cs
@@ -1,10 +1,12 @@
-﻿Console.WriteLine("Write first number");
+﻿using TLibrary;
+
+Console.WriteLine("Write first number");
 double a = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Write second number");
 double b = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Write third number");
 double c = Convert.ToDouble(Console.ReadLine());
 
-double result = SLibrary.Lib.Add(a, b, c);
+double result = TLibrary.T.Add(a, b, c);
 Console.WriteLine(result);
 Console.ReadLine();
---
+++ TLibrary/T.cs
@@ -0,0 +1,8 @@
+﻿namespace TLibrary
+{
+    public class T
+    {
+        public static double Add(double a, double b, double c)
+        { return (a + b + c) / 3; }
+    }
+}
