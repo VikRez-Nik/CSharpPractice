df4c1014192ab5f1fe6588d2cb9a166cae2b4465
DESKTOP-PF5B1E8\spark <viktorreznik116@gmail.com>
2022-07-27 12:44:02 +0300

    27.07.2022 12.43

--- AverageLib/Program.cs
+++ AverageLib/Program.cs
@@ -1,3 +1,10 @@
-﻿double result = Library.Lib.Add(15, 10,10 );
+﻿Console.WriteLine("Write first number");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Write second number");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Write third number");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double result = Library.Lib.Add(a, b, c );
 Console.WriteLine(result);
 Console.ReadLine();
