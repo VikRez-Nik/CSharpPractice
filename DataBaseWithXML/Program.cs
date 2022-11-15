43051744590edac17ea8eb0a36fd8cf2977c8ebc
DESKTOP-PF5B1E8\spark <viktorreznik116@gmail.com>
2022-11-15 09:43:01 +0300

    no message

---
+++ DataBaseWithXML/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Serialization;
+
+Person[] people = new Person[]
+{
+    new Person("Tom", 37),
+    new Person("Bob", 41)
+};
+
+XmlSerializer formatter = new XmlSerializer(typeof(Person[]));
+// сохранение массива в файл
+using (FileStream fs = new FileStream("person.xml", FileMode.OpenOrCreate))
+{
+    formatter.Serialize(fs, people);
+}
+// восстановление массива из файла
+using (FileStream fs = new FileStream("person.xml", FileMode.OpenOrCreate))
+{
+    Person[]? newpeople = formatter.Deserialize(fs) as Person[];
+
+    if (newpeople != null)
+    {
+        foreach (Person person in newpeople)
+        {
+            Console.WriteLine($"Name: {person.Name} --- Age: {person.Age}");
+        }
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; } = "Undefined";
+    public int Age { get; set; } = 1;
+
+    public Person() { }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
